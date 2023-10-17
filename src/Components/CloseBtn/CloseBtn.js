de7ca7e5cdd93aacf70f678ecd5575bb07688d89
Egor Ondatrov <realitzegor@gmail.com>
2023-10-17 20:06:18 +0300

    closeBtn fix

--- src/Components/CloseBtn/CloseBtn.js
+++ src/Components/CloseBtn/CloseBtn.js
@@ -1,7 +1,7 @@
 import CloseIcon from "../icons/CloseIcon";
 
 export default function CloseBtn({ ...rest }) {
-  const { className, onClick, children = CloseIcon } = rest;
+  const { className, onClick, children = <CloseIcon /> } = rest;
   return (
     <button className={className} onClick={onClick}>
       {children}
